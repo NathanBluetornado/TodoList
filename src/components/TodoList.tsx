a13780ae318ec7332f3a9d3049e0fe2009c940e0
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2025-02-17 01:32:44 -0500

    Add and implement button to add item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/TodoList.css'
+
 import { ITodoList, TodoListParams } from '../types/TodoList';
+import { ITodoItem } from '../types/TodoItem';
 
 import TodoItem from '../components/TodoItem';
 
-export default function List({ title, items, setList }: TodoListParams) {
+export default function TodoList({ title, items, setList }: TodoListParams) {
+
+    const [addButtonVisibility , setAddButtonVisibility] = useState<boolean>(false)
+    const toggleAddButtonVisibility = () => setAddButtonVisibility( (v:boolean) => !v)
+
+    const addButtonOnSubmit = (e: any) => {
+        e.preventDefault();
+
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+
+        toggleAddButtonVisibility();
+        addItem({title: formJson.addListItem.toString(), complete: false})
+    }
+
+    const addItem = (item : ITodoItem) => {
+        setList( (curList : ITodoList) => {
+            let newList : ITodoList = {title: curList.title, items: curList.items};
+            newList.items = newList.items.concat(item);
+            return newList;
+        });
+    }
 
     const toggleChecked = (index:number, value:boolean) => {
         setList( (curList : ITodoList) => {
@@ -15,7 +41,13 @@ export default function List({ title, items, setList }: TodoListParams) {
 
     return (
         <div className="list">
-            <h2>{title}</h2>
+            <div>
+                <h2>{title}</h2>
+                <button onClick={()=> toggleAddButtonVisibility() }>+</button>
+                { addButtonVisibility && <form onSubmit={addButtonOnSubmit}>
+                    <input name="addListItem"/>
+                </form>}
+            </div>
             <ul>
             {
                 items.map( (item, index) => {
